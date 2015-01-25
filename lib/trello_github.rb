776fad4238025b82a3e02afb3edbe4a3ca4bdfea
LFDM <1986gh@gmail.com>
2015-01-25 11:13:10 +0100

    Start with main class

--- lib/trello_github.rb
+++ lib/trello_github.rb
@@ -1,5 +1,16 @@
+require 'json'
+require 'trello'
 require "trello_github/version"
+require "trello_github/config"
+require 'trello_github/git_actions'
 
-module TrelloGithub
-  # Your code goes here...
+class TrelloGithub
+  attr_reader :config
+  include GitActions
+
+  def initialize
+    @config = Config.new
+  end
+
+  private
 end
